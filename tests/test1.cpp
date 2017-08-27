08c70f1ebc412adaad38bd686de0d9f5ed94d79f
PLeLuron <pierre.leluron@gmail.com>
2017-08-27 23:45:47 +0200

    Unit test mockup

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,28 +2,82 @@
 #include <ostream>
 #include <fstream>
 #include <sstream>
-#include <map>
+#include <vector>
+#include <string>
+#include <functional>
+
 #include <SHAUN/parser.hpp>
 #include <SHAUN/printer.hpp>
 
-int main(void)
+using namespace std;
+
+bool test1()
 {
     try
     {
+        // Parse file
         shaun::parser p;
-        std::ifstream file("../resources/test.shaun");
-        std::stringstream buffer; 
+        ifstream file("../resources/test.shaun");
+        stringstream buffer; 
         buffer << file.rdbuf();
+        shaun::object obj1 = p.parse(buffer.str());
 
-        shaun::object obj = p.parse(buffer.str());
+        // Generate shaun file
+        stringstream output1;
+        shaun::printer pr1(&output1);
+        pr1.visit(obj1);
 
-        shaun::printer pr(&std::cout);
-        pr.visit(obj);
+        // Parse generated file
+        shaun::object obj2 = p.parse(output1.str());
+        // Regenerate shaun file
+        stringstream output2;
+        shaun::printer pr2(&output2);
+        pr2.visit(obj2);
+
+        // Compare both files
+        return output1.str() == output2.str();
     }
     catch (shaun::parse_error e)
     {
         std::cout << e << std::endl;
+        return false;
+    }
+}
+
+#define MAKE_TEST(name) (UnitTest(#name, name))
+
+class UnitTest
+{
+public:
+    UnitTest(const string &name, const function<bool(void)> &test) : _name{name}, _test{test} {}
+    const string &getName() const {return _name;}
+    const function<bool(void)> &getTest() const {return _test;}
+private:
+    string _name;
+    function<bool(void)> _test;
+};
+
+int main(void)
+{
+    const vector<UnitTest> unitTests = {
+        MAKE_TEST(test1)
+    };
+
+    int failedNum = 0;
+    for (const auto &test : unitTests)
+    {
+        const bool result = test.getTest()();
+        cout << "Test " << test.getName() << (result?" successful":" failed") << endl;
+        failedNum += result?0:1;
     }
 
-    return 0;
+    cout << "-----------------------" << endl;
+    if (failedNum)
+    {
+        cout << failedNum << " tests failed" << endl;
+    }
+    else
+    {
+        cout << "All tests successful" << endl;
+    }
 }
